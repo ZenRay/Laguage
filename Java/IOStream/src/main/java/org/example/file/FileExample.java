f2426c8f8a145902a4512e3e34e0821fbfee5a03
ZenRay <jake2011ren@gmail.com>
2024-05-27 11:14:25 +0800

    Java: Update File Example Demo

--- Java/IOStream/src/main/java/org/example/file/FileExample.java
+++ Java/IOStream/src/main/java/org/example/file/FileExample.java
@@ -1,2 +1,40 @@
-package org.example.file;public class FileExample {
+package org.example.file;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileExample {
+    public static void main(String[] args) {
+
+        System.out.println(
+                "File package contain path separator: '" + File.pathSeparator +
+                        "' file sperator: '" + File.separator + "'"
+        );
+        //
+        File file = new File("./data/test.txt");
+        System.out.println("File path: " + file.getAbsolutePath());
+        System.out.println("File name: " + file.getName());
+        // 相对路径
+        System.out.println("File path: " + file.getPath());
+
+        // 检查父目录是否存在
+        File parentPath = new File(file.getParent());
+        if(! parentPath.exists()){
+            System.out.println("Parent path does not exist, create directory!");
+            parentPath.mkdirs();
+        }
+
+
+        // 创建文件
+        try {
+            if (! file.exists()) {
+                System.out.println("File dosen't exist, then create file");
+                file.createNewFile();
+            }else {
+                System.out.println("File exist!");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
